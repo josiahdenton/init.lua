7b2f4231eac7904c4654b9f7d45f3005a7f49a1e
josiahdenton <jfranklindenton@gmail.com>
2023-07-09 14:50:32 -0400

    cleanup + harpoon

--- after/plugin/null-ls-setup.lua
+++ after/plugin/null-ls-setup.lua
@@ -4,6 +4,7 @@ null_ls.setup({
     sources = {
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.completion.spell,
+        null_ls.builtins.formatting.autopep8,
+        null_ls.builtins.diagnostics.flake8
     },
 })
--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,2 +1,11 @@
+local api = require("nvim-tree.api")
+
 -- empty setup using defaults
 require("nvim-tree").setup()
+
+local function open_nvim_tree()
+    api.tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter "}, { callback = open_nvim_tree })
+vim.api.nvim_set_keymap("n", "<Leader>?", ":NvimTreeFindFile<CR>", { noremap = true })
