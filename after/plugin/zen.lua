e754a559f58ed40ae4541291a779c5636f7d45ba
Josiah Denton <jfdenton@vt.edu>
2023-08-06 10:42:44 -0400

    switch zen to have line numbers

--- after/plugin/zen.lua
+++ after/plugin/zen.lua
@@ -1,19 +1,34 @@
-require("true-zen").setup({
-    modes = {
-        ataraxis = {
-            minmum_writing_area = {
-                width = 120,
-                height = 44,
-            }
-        }
+-- require("true-zen").setup({
+--     modes = {
+--         ataraxis = {
+--             minmum_writing_area = {
+--                 width = 120,
+--                 height = 44,
+--             }
+--         }
+--     }
+-- })
+--
+-- local api = vim.api
+--
+-- api.nvim_set_keymap("n", "<leader>zn", ":TZNarrow<CR>", {})
+-- api.nvim_set_keymap("v", "<leader>zn", ":'<,'>TZNarrow<CR>", {})
+-- -- api.nvim_set_keymap("n", "<leader>zf", ":TZFocus<CR>", {})
+-- -- api.nvim_set_keymap("n", "<leader>zm", ":TZMinimalist<CR>", {})
+-- api.nvim_set_keymap("n", "<leader>zz", ":TZAtaraxis<CR>", {})
+--
+
+vim.keymap.set("n", "<leader>zz", function()
+    require("zen-mode").setup {
+        window = {
+            width = 150,
+            options = { }
+        },
     }
-})
+    require("zen-mode").toggle()
+    vim.wo.wrap = false
+    vim.wo.number = true
+    vim.wo.rnu = true
+end)
 
-local api = vim.api
-
-api.nvim_set_keymap("n", "<leader>zn", ":TZNarrow<CR>", {})
-api.nvim_set_keymap("v", "<leader>zn", ":'<,'>TZNarrow<CR>", {})
--- api.nvim_set_keymap("n", "<leader>zf", ":TZFocus<CR>", {})
--- api.nvim_set_keymap("n", "<leader>zm", ":TZMinimalist<CR>", {})
-api.nvim_set_keymap("n", "<leader>zz", ":TZAtaraxis<CR>", {})
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,7 +8,7 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     use 'theprimeagen/harpoon'
-    use 'Pocco81/true-zen.nvim'
+    use 'folke/zen-mode.nvim'
     use { 'akinsho/git-conflict.nvim', tag = "*", config = function()
         require('git-conflict').setup()
     end }
