9f7f91acffdefa7fae9c79015d25b923c28e2483
Josiah Denton <jfdenton@vt.edu>
2023-08-11 07:07:08 -0400

    init support for c/cpp

--- lua/remap.lua
+++ lua/remap.lua
@@ -6,3 +6,17 @@ end)
 
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
+
+-- vim.cmd([[
+-- " " Copy to clipboard
+-- vnoremap  <leader>y  "+y
+-- nnoremap  <leader>Y  "+yg_
+-- nnoremap  <leader>y  "+y
+-- nnoremap  <leader>yy  "+yy
+--
+-- " " Paste from clipboard
+-- nnoremap <leader>p "+p
+-- nnoremap <leader>P "+P
+-- vnoremap <leader>p "+p
+-- vnoremap <leader>P "+P
+-- ]])
