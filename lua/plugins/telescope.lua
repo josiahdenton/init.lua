71d326db155c1aafe6b3a1efe69cbd648ea8ea86
Josiah Denton <jfdenton@vt.edu>
2023-09-26 11:25:30 -0400

    improve telescope perf with fzf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,10 @@ return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.3',
     -- or                              , branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    },
     config = function()
         require('telescope').setup({
             defaults = {
@@ -13,6 +16,15 @@ return {
                 diagnostics = {
                     theme = 'ivy',
                 },
+            },
+            extensions = {
+                fzf = {
+                    fuzzy = true,                   -- false will only do exact matching
+                    override_generic_sorter = true, -- override the generic sorter
+                    override_file_sorter = true,    -- override the file sorter
+                    case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+                    -- the default case_mode is "smart_case"
+                }
             }
         })
         -- themes
@@ -29,6 +41,9 @@ return {
                 prompt_title = false
             })
         end
+
+        -- extensions
+        require('telescope').load_extension('fzf')
 
         -- setup key maps
         local builtin = require('telescope.builtin')
