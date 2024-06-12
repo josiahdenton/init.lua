d435634b406045cb1f695faff2f082178b320d84
Josiah Denton <jfranklindenton@gmail.com>
2024-06-12 12:37:30 -0400

    chore: diagnostics in split as well

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -67,12 +67,10 @@ return {
                     diagnostics = {
                         focus = true,
                         win = {
-                            type = "float",
-                            relative = "editor",
-                            border = "rounded",
-                            position = { 0.5, 0.5 },
-                            size = { width = 0.5, height = 0.4 },
-                            zindex = 200,
+                            type = "split",
+                            relative = "win",
+                            position = "left",
+                            size = 0.2,
                         },
                     },
                     symbols = {
