7f784f1a7cb98e192fed21cb877414d49beae837
Josiah Denton <jfranklindenton@gmail.com>
2024-06-18 20:31:57 -0400

    chore: floating window for tasks

--- lua/core/extensions/buffers.lua
+++ lua/core/extensions/buffers.lua
@@ -1,5 +1,57 @@
 local M = {}
 
+---@class ToggleTaskBufferOptions
+---@field width? integer
+local default = {
+    width = 0.75,
+}
+
+local bufnr = -1
+local open = false
+local border = require("core.ui.style").rounded_border()
+
+-- TODO: make this a floating window
+
+---@param opts? ToggleTaskBufferOptions
+M.toggle_tasks_buffer = function(opts)
+    opts = opts or default
+
+    if open and bufnr > 0 then
+        open = false
+        vim.api.nvim_buf_delete(bufnr, { unload = true })
+        return -- return early, don't do anything else
+    end
+
+    local win = vim.api.nvim_get_current_win()
+    if bufnr < 0 then -- if buffer not set, create a new scratch buffer
+        bufnr = vim.api.nvim_create_buf(true, true)
+    end
+
+    local win_width = vim.api.nvim_win_get_width(win)
+    local win_height = vim.api.nvim_win_get_height(win)
+    local width = math.floor(opts.width * win_width)
+
+    -- center 
+    local gheight = vim.api.nvim_list_uis()[1].height
+    local gwidth = vim.api.nvim_list_uis()[1].width
+
+    local float_win = vim.api.nvim_open_win(bufnr, false, { -- create a new split in the current window and put the new buffer in that win
+        title = "Tasks",
+        border = border,
+        relative = "win",
+        row = (gheight - 15) * 0.5, -- row is height
+        col = (gwidth - width) * 0.5, -- col is width
+        win = win,
+        height = 15,
+        width = width,
+    })
+    vim.api.nvim_buf_call(bufnr, function()
+        vim.cmd("e ~/notes/index.norg")
+    end)
+    vim.api.nvim_set_current_win(float_win)
+    open = true -- task list is now open
+end
+
 -- example of persisting buffer id then closing it...
 -- local id = vim.api.nvim_create_augroup("startup", {
 --   clear = false
@@ -34,45 +86,23 @@ local M = {}
 --       end
 --     end
 --   end, { silent = true, desc = 'Close unused buffers' })
---
--- TODO: enable this to toggle the task list buffer!
--- use the example above to track bufnr
-
-local bufnr = -1
-M.toggle_tasks_buffer = function()
-    -- if bufnr > 0 then -- created, now close it
-    --     vim.cmd('bd ' .. tostring(bufnr))
-    --     bufnr = -1
-    -- end
-    -- io.open
-    -- io.write
-    -- io.close
-
-    -- 5.1  Basic Functions                                           *lua-lib-core*
-    -- 5.7, 5.8
-    local win = vim.api.nvim_get_current_win()
-    if bufnr < 0 then
-        bufnr = vim.api.nvim_create_buf(true, true) -- create a new scratch buffer to throwaway
-    end
-
-    -- vim.api.nvim_win_set_buf(win, bufnr)
-    -- vim.lsp.buf_attach_client
-    -- vim.lsp.get_clients
-    -- vim.lsp.start
-    -- lua vim.api.nvim_buf_call(buf, function() vim.cmd('edit /absolute/path/to/a/file.lua') end)
-    -- local file_type = vim.bo.filetype
-    -- vim.api.nvim_set_option_value("filetype", "nor", { buf = bufnr }) -- set file type of buffer to markdown
-    vim.api.nvim_open_win(bufnr, false, { -- create a new split in the current window and put the new buffer in that win
-        split = "left",
-        win = win,
-        width = 60,
-    })
-    vim.api.nvim_buf_call(bufnr, function()
-        -- vim.cmd("Neorg workspace notes")
-        vim.cmd("e ~/notes/index.norg")
-    end)
-end
 
 -- vim.print(vim.bo.buftype)
+-- vim.api.nvim_win_set_buf(win, bufnr)
+-- vim.lsp.buf_attach_client
+-- vim.lsp.get_clients
+-- vim.lsp.start
+-- lua vim.api.nvim_buf_call(buf, function() vim.cmd('edit /absolute/path/to/a/file.lua') end)
+-- local file_type = vim.bo.filetype
+-- vim.api.nvim_set_option_value("filetype", "nor", { buf = bufnr }) -- set file type of buffer to markdown
+-- if bufnr > 0 then -- created, now close it
+--     vim.cmd('bd ' .. tostring(bufnr))
+--     bufnr = -1
+-- end
+-- io.open
+-- io.write
+-- io.close
+-- 5.1  Basic Functions                                           *lua-lib-core*
+-- 5.7, 5.8
 
 return M
