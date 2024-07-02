55b4b397c6ebda650ca1bdd36c494b0329b1c2f1
Josiah Denton <jfranklindenton@gmail.com>
2024-07-02 10:04:21 -0400

    chore: disable nvim-java

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,16 +1,16 @@
 return {
-    {
-        "nvim-java/nvim-java",
-        config = function()
-            require("java").setup()
-        end,
-    },
+    -- {
+    --     "nvim-java/nvim-java",
+    --     config = function()
+    --         require("java").setup()
+    --     end,
+    -- },
     -- LSP Configuration & Plugins
     {
         "neovim/nvim-lspconfig",
         dependencies = {
             -- nvim java must run before lspconfig setup
-            "nvim-java",
+            -- "nvim-java",
             -- ensure nvim-cmp plugins are loaded before setting up the config
             "nvim-cmp",
             -- Automatically install LSPs to stdpath for neovim
