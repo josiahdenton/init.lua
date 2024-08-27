1622aaa3da20b533cc035e6a7d7cfc7be0b3226a
Josiah Denton <jfranklindenton@gmail.com>
2024-08-27 10:45:31 -0400

    chore: fix telescope width

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -104,23 +104,23 @@ return {
             end)
 
             vim.keymap.set("n", "gI", function()
-                builtins.lsp_implementations(themes.get_cursor())
+                builtins.lsp_implementations(themes.get_cursor({ layout_config = { width = 110 } }))
             end, { desc = "telescope: go to lsp_implementations" })
 
             vim.keymap.set("n", "gi", function()
-                builtins.lsp_incoming_calls(themes.get_cursor())
+                builtins.lsp_incoming_calls(themes.get_cursor({ layout_config = { width = 110 } }))
             end, { desc = "telescope: go to lsp_incoming_calls" })
 
             vim.keymap.set("n", "go", function()
-                builtins.lsp_outgoing_calls(themes.get_cursor())
+                builtins.lsp_outgoing_calls(themes.get_cursor({ layout_config = { width = 110 } }))
             end, { desc = "telescope: go to lsp_outgoing_calls" })
 
             vim.keymap.set("n", "gd", function()
-                builtins.lsp_definitions(themes.get_cursor())
+                builtins.lsp_definitions(themes.get_cursor({ layout_config = { width = 110 } }))
             end, { desc = "telescope: go to lsp_definitions" })
 
             vim.keymap.set("n", "gr", function()
-                builtins.lsp_references(themes.get_cursor())
+                builtins.lsp_references(themes.get_cursor({ layout_config = { width = 110 } }))
             end, { desc = "telescope: go to lsp_references" })
         end,
     },
